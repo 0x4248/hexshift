3f3a9ea4980217dcf11568f7972de4f0d7e44d3d
awesomelewis2007 <awesomelewis2007@gmail.com>
2023-02-11 12:34:55 +0000

    Add passses times argument

--- encrypt.py
+++ encrypt.py
@@ -3,12 +3,12 @@ def generate_shifter():
     shifter = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
     random.shuffle(shifter)
     return shifter
-def encrypt(data):
+def encrypt(data,shift_times):
     data = data.hex().upper()
     iterate = 0
     shifter = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
     shifters = []
-    for i in range(100):
+    for i in range(shift_times):
         output = ""
         shift_list = generate_shifter()
         shifters.append(shift_list)
