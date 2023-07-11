274a2c5abcf029d53243c03f66ece9f9138e4009
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-07-11 15:24:17 +0100

    Format comments
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- src/__main__.py
+++ src/__main__.py
@@ -23,10 +23,11 @@ banner = r""" **      ** ******** **     **  ******** **      ** ** ******** ***
 
 def shifter_mode_menu():
     """ Shows the shifter mode menu
-
+    
     Returns:
         int: Number of passes to use
     """
+
     while True:
         print(
             "Please select an option to adjust the security level. Increasing the number of passes will improve security but also increase processing time and storage space usage"
@@ -69,6 +70,7 @@ def shifter_mode_menu():
 def cli_encrypt():
     """ Shows the encrypt menu
     """
+
     while True:
         print("Please chose an option")
         print("1) Encrypt text")
@@ -135,6 +137,7 @@ def cli_encrypt():
 def cli_decrypt():
     """ Shows the decrypt menu
     """
+
     file_location = input("Enter file to decrypt>")
     key_location = input("Enter keyfile>")
     try:
@@ -178,6 +181,7 @@ def cli_decrypt():
 if __name__ == "__main__":
     """ Main function
     """
+
     print(Fore.CYAN + banner + Style.RESET_ALL)
     print("Welcome to hexshift CLI V" + version)
     while True:
